409c33004481ad96565d954f39f5738c4665d23b
Florence Meier <florence.meier@swisscom.com>
2018-08-25 09:57:07 +0200

    Added tabs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+      <li class="nav-item">
+        <a class="nav-link active" id="map-tab" data-toggle="tab" href="#map" role="tab" aria-controls="map" aria-selected="true">MAP</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" id="challenge-tab" data-toggle="tab" href="#challenge" role="tab" aria-controls="challenge" aria-selected="false">CHALLENGE</a>
+      </li>
+    </ul>
+    <div class="tab-content" id="myTabContent">
+      <div class="tab-pane fade show active" id="map" role="tabpanel" aria-labelledby="map-tab">
+        HERE COMES MAP-COMPONENT
+      </div>
+      <div class="tab-pane fade" id="challenge" role="tabpanel" aria-labelledby="challenge-tab">
+        HERE COMES CHALLENGE-COMPONENT
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,7 +24,6 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'MAINPAGE'
     }
   }
 }
--- src/components/header.vue
+++ src/components/header.vue
@@ -14,6 +14,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Challenges</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Achievements</a>
+                </li>
             </ul>
         </div>
     </nav>
